f0c56c2a996126e34fe8bd89b58ef32e2ed57a20
Rasads7 <rafasilva1807@gmail.com>
2025-09-03 19:18:07 -0300

    RotasAtualizadas

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ from db import db
 import hashlib
 
 app = Flask(__name__)
-app.secret_key = 'testelogin'
+app.secret_key = 'teste'
 lm = LoginManager(app)
-lm.Login_view = 'login'
+lm.login_view = 'login'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 db.init_app(app)
 
-def hash (txt):
+def hash(txt):
     hash_obj = hashlib.sha256(txt.encode('utf-8'))
     return hash_obj.hexdigest()
-
-
 
 @lm.user_loader
 def user_loader(id):
@@ -23,28 +21,28 @@ def user_loader(id):
     return usuario
 
 @app.route('/')
-@login_required
 def home():
-    print(current_user)
-    return render_template('home.html')
+    if current_user.is_authenticated:
+        return render_template('homecomlogin.html')
+    else:
+        return render_template('homesemlogin.html')
 
 @app.route('/campanhas')
 @login_required
 def campanhas():
     return render_template('campanhas.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
-        nome = request.form['nomeForm']
-        senha = request.form['senhaForm']
+        email = request.form['EmailFormLogin']
+        senha = request.form['SenhaFormLogin']
 
-        user = db.session.query(Usuario).filter_by(nome=nome, senha=hash(senha)).first()
+        user = db.session.query(Usuario).filter_by(email=email, senha=hash(senha)).first()
         if not user:
-            return 'Nome ou senha incorretos'
+            return 'Email ou senha incorretos'
         
         login_user(user)
         return redirect(url_for('home'))
@@ -54,10 +52,15 @@ def registrar():
     if request.method == 'GET':
         return render_template('registrar.html')
     elif request.method == 'POST':
-        nome = request.form['nomeForm']
-        senha = request.form['senhaForm']
+        nome = request.form['NomeFormRegistrar']
+        email = request.form['EmailFormRegistrar']
+        senha = request.form['SenhaFormRegistrar']
 
-        novo_usuario = Usuario(nome=nome, senha=hash(senha))
+        existente = db.session.query(Usuario).filter_by(email=email).first()
+        if existente:
+            return 'Este email já está cadastrado.'
+
+        novo_usuario = Usuario(nome=nome, email=email, senha=hash(senha))
         db.session.add(novo_usuario)
         db.session.commit()
 
@@ -71,28 +74,43 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/formulario', methods=['GET', 'POST'])
+@app.route('/formulariodoar', methods=['GET', 'POST'])
 @login_required  
-def formulario():
+def formulariodoar():
     if request.method == 'GET':
-        return render_template('formulario.html')
+        return render_template('formulariodoar.html')
     
     elif request.method == 'POST':
-        tipo = request.form['tipo']
-        nome = request.form['nome']
-        telefone = request.form['telefone']
-        endereco = request.form['endereco']
+        tipo = request.form['tipoFormDoar']
+        nome = request.form['nomeFormDoar']
+        telefone = request.form['telefoneFormDoar']
+        endereco = request.form['enderecoFormDoar']
 
         if tipo == 'cesta':
-            local_entrega = request.form['local_entrega']
-            data_entrega = request.form['data_entrega']
+            local_entrega = request.form['local_entregaFormDoar']
+            data_entrega = request.form['data_entregaFormDoar']
             return f'Cesta Básica registrada para {nome}'
 
         elif tipo == 'monetaria':
-            valor = request.form['valor']
+            valor = request.form['valorFormDoar']
             return f'Doação monetária de R${valor} registrada para {nome}'
 
         return 'Tipo de doação inválido'
+
+@app.route('/formularioreceber', methods=['GET', 'POST'])
+@login_required  
+def formularioreceber():
+    if request.method == 'GET':
+        return render_template('formularioreceber.html')
+    
+    elif request.method == 'POST':
+        nome = request.form['nomeFormReceber']
+        telefone = request.form['telefoneFormReceber']
+        endereco = request.form['enderecoFormReceber']
+        qtd_pessoas = request.form['qtd_pessoasFormReceber']
+        necessidades = request.form['necessidadesFormReceber']
+
+        return f'Cadastro para receber doação: {nome}, {telefone}, {endereco}, {qtd_pessoas} pessoas. Necessidades: {necessidades}'
 
 if __name__ == '__main__':
     with app.app_context():
--- models.py
+++ models.py
@@ -5,5 +5,6 @@ class Usuario(UserMixin, db.Model):
     __tablename__ = 'usuarios'
 
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(30), unique=True)
-    senha = db.Column(db.String())
+    nome = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha = db.Column(db.String(), nullable=False)
