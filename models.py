26737230a110c7054097547e64b1ad797eea6556
Pablo Gomes Sepúlveda <82807976+MrFrordoff@users.noreply.github.com>
2025-09-13 03:29:33 -0300

    models.py

--- models.py
+++ models.py
@@ -8,3 +8,13 @@ class Usuario(UserMixin, db.Model):
     nome = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(), nullable=False)
+
+class Campanha(db.Model):
+    __tablename__ = 'campanhas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
+    criador_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+
+    criador = db.relationship('Usuario', backref='campanhas')
